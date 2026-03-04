384faac7516c74c34f2735d242ecb811656831c6
Shadmanee <tshadmanee@gmail.com>
2026-03-04 19:59:23 +0600

    last try for vercel with updated baseName

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const baseName = import.meta.env.BASE_URL;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={baseName}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
